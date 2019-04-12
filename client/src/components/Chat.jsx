c7b95120650d22f0de9be0128785eafbe9db96cc
hajdinim <hajdinim@coventry.ac.uk>
2019-04-12 01:40:04 +0100

    design 40% done!

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -4,10 +4,15 @@ export default class Chat extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      username: this.props
+      username: this.props,
+      message: ''
     }
   }
+  sendMessage = () => {
+    console.log('lol')
+  }
   render() {
+    const { message } = this.state
     return (
       <div class="body">
         <div class="user">
@@ -27,7 +32,7 @@ export default class Chat extends Component {
           </div>
           <div class="usersOnlineDiv">
             <div class="usersOnline">
-              <img src={require('../photos/userPic.png')} />
+              <img src={require('../photos/userPic.png')} alt="lol" />
               <div>
                 <h4 class="chatName">Dolores La Flipo</h4>
                 <p class="chatMessage">Hello there</p>
@@ -36,7 +41,7 @@ export default class Chat extends Component {
           </div>
           <div class="usersOnlineDiv">
             <div class="usersOnline">
-              <img src={require('../photos/userPic.png')} />
+              <img src={require('../photos/userPic.png')} alt="lol" />
               <div>
                 <h4 class="chatName">Dolores La Flipo</h4>
                 <p class="chatMessage">Hello there</p>
@@ -45,9 +50,24 @@ export default class Chat extends Component {
           </div>
         </div>
         {/* Chat Grid */}
-        <div>
+        <div class="chatGrid">
+          <div>this is the info</div>
+          <div class="messageInfo" />
           <div>Dolores La Flipe</div>
           <h2>hey</h2>
+          <div class="sendMessage">
+            <input
+              type="text"
+              placeholder="Type a message..."
+              onChange={e => {
+                this.setState({ message: e.target.value })
+              }}
+              value={this.state.message}
+            />
+            <button type="submit" disabled={message ? false : true} onClick={this.sendMessage}>
+              Send
+            </button>
+          </div>
         </div>
       </div>
     )
